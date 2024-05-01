c0ef33c080a02a9f11d81a37913bfbc6661caf2e
thomasGarau <thomas.garau8@gmail.com>
2024-05-01 14:14:51 +0200

    qee

--- src/middlewares/verifyAuthorisation.js
+++ src/middlewares/verifyAuthorisation.js
@@ -110,8 +110,7 @@ const verifyIsTeacher = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const num_etudiant = parseInt(decoded.num_etudiant);
-        if (num_etudiant < 200000 ) {
+        if (decoded.role == "enseignant") {
             return next();  
         } else {
             return res.status(403).send('Accès non autorisé. Vous devez être un enseignant.');
